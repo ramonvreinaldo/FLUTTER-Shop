724ca6e900b494f188547965ea13f6a270a1e045
Ramon Reinaldo <ramonvreinaldo@gmail.com>
2024-08-29 09:46:25 +0100

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const userFavoritesUrl =
+      'https://shop-954ca-default-rtdb.firebaseio.com/userFavorites';
+  static const productBaseUrl =
+      'https://shop-954ca-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-954ca-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyBkqd7GRt26P_MPU_UsUS2TQ5CjcLTYRLA';
+}
